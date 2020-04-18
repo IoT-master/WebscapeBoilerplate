e7828b0ffc11b5040569ed1361672d01acb0c4e2
Senh Mo Chuang <senhmo@Senhs-MacBook-Pro.local>
2020-04-18 02:44:46 -0500

    Added Selenium Actions

--- Webscraper.py
+++ Webscraper.py
@@ -1,9 +1,16 @@
+#!./.env/bin/python3
 from selenium.webdriver import Chrome, ChromeOptions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+# from selenium.webdriver import Firefox, FirefoxProfile
+# from selenium.webdriver import PhantomJS
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from pathlib import Path
 import os
+
 
 
 class LoginClass(object):
@@ -28,6 +35,67 @@ class LoginClass(object):
 
     def logging_in(self, url):
         self.browser.get(url)
+
+    def wait_until_css_element_object_found(self, css_param, wait_time=10):
+        wait = WebDriverWait(self.browser, wait_time)
+        wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, css_param)))
+
+    def scroll_down_one_page(self, browser_object, lines_down):
+        browser_object.execute_script("window.scrollTo(0, " + str(lines_down) + ")")
+
+    def drag_and_drop(self, browser_object, source_element_object, destination_element_object):
+        ActionChains(browser_object).drag_and_drop(source_element_object, destination_element_object).perform()
+
+    def hover_over(self, browser_object, element_object):
+        ActionChains(browser_object).move_to_element(element_object).perform()
+
+    def is_present(self, element_object):
+        try:
+            # browser.find_element_by_css_selector("div")
+            if element_object.is_displayed():
+                return True
+        except:
+            return False
+
+    def update_browser(self, browser_object):
+        for handle in browser_object.window_handles:
+            browser_object.switch_to.window(handle)
+
+    def wait_until_name_element_object_found(self, browser_object, name_param, wait_time=10):
+        wait = WebDriverWait(browser_object, wait_time)
+        wait.until(EC.visibility_of_element_located((By.NAME, name_param)))
+        # sleep(1)
+
+    def wait_until_partial_link_text_element_object_found(self, browser_object, partial_link_text, wait_time=10):
+        wait = WebDriverWait(browser_object, wait_time)
+        wait.until(EC.visibility_of_element_located((By.PARTIAL_LINK_TEXT, partial_link_text)))
+        # sleep(1)
+
+    def wait_until_class_name_element_object_found(self, browser_object, class_name, wait_time=10):
+        wait = WebDriverWait(browser_object, wait_time)
+        wait.until(EC.visibility_of_element_located((By.CLASS_NAME, class_name)))
+        # sleep(1)
+
+    def wait_until_id_element_object_found(self, browser_object, id_object, wait_time=10):
+        wait = WebDriverWait(browser_object, wait_time)
+        wait.until(EC.visibility_of_element_located((By.ID, id_object)))
+        # sleep(1)
+
+    def wait_until_partial_link_text_object_found(self, browser_object, id_object, wait_time=10):
+        wait = WebDriverWait(browser_object, wait_time)
+        wait.until(EC.visibility_of_element_located((By.PARTIAL_LINK_TEXT, id_object)))
+        # sleep(1)
+
+    def multi_select_in_list(self, element_objects, labels):
+        for option in element_objects:
+            if option.text in labels:
+                option.click()
+
+    def select_in_list(self, element_objects, labels):
+        for option in element_objects:
+            if option.text in labels:
+                option.click()
+                break
 
 
 if __name__ == '__main__':
