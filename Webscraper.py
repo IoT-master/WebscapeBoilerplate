d68d3b7de182eb0cdc1dac3c41e96f57c0bceb51
Senh Mo Chuang <senhmo@gmail.com>
2021-08-10 12:33:39 -0500

    Integrating Actions to Chrome

--- Webscraper.py
+++ Webscraper.py
@@ -12,7 +12,6 @@ from pathlib import Path
 import os
 from time import sleep
 
-
 def delay_before(delay):
     def wrap(f):
         def wrapped_f(*args):
@@ -30,7 +29,7 @@ def delay_after(delay):
     return wrap
 
 
-class DelayBefore(object):
+class DelayBefore:
     def __init__(self, delay):
         self.delay = delay
 
@@ -42,16 +41,18 @@ class DelayBefore(object):
         return wrapper
 
 
-class LoginClass(object):
+class MyChrome(Chrome):
 
     def __init__(self, incognito=True, headless=False, brave=False):
+        
         options = ChromeOptions()
         options.add_argument("disable-extensions")
         if incognito:
             options.add_argument("incognito")
         if headless:
             options.add_argument("headless")
-
+        # https://stackoverflow.com/questions/64927909/failed-to-read-descriptor-from-node-connection-a-device-attached-to-the-system
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
         # options.add_argument("disable-gpu")
         # options.add_argument('window-size=1200x1200')
         # options.add_argument("remote-debugging-port=9222")
@@ -69,26 +70,26 @@ class LoginClass(object):
                 options.binary_location = "/Applications/Brave Browser.app/Contents/MacOS/Brave Browser"
             path_to_chrome = str(
                 Path('./ChromeDrivers/Linux/chromedriver').absolute())
-        self.browser = Chrome(path_to_chrome, options=options)
+        super().__init__(path_to_chrome, options=options)
 
     def logging_in(self, url):
-        self.browser.get(url)
+        self.get(url)
 
     def wait_until_css_element_object_found(self, css_param, wait_time=10):
-        wait = WebDriverWait(self.browser, wait_time)
+        wait = WebDriverWait(self, wait_time)
         wait.until(EC.visibility_of_element_located(
             (By.CSS_SELECTOR, css_param)))
 
     def scroll_down_one_page(self, lines_down):
-        self.browser.execute_script(
+        self.execute_script(
             "window.scrollTo(0, " + str(lines_down) + ")")
 
     def drag_and_drop(self, source_element_object, destination_element_object):
-        ActionChains(self.browser).drag_and_drop(
+        ActionChains(self).drag_and_drop(
             source_element_object, destination_element_object).perform()
 
     def hover_over(self, element_object):
-        ActionChains(self.browser).move_to_element(element_object).perform()
+        ActionChains(self).move_to_element(element_object).perform()
 
     def is_present(self, element_object):
         try:
@@ -99,29 +100,29 @@ class LoginClass(object):
             return False
 
     def update_browser(self):
-        for handle in self.browser.window_handles:
-            self.browser.switch_to.window(handle)
+        for handle in self.window_handles:
+            self.switch_to.window(handle)
 
     def wait_until_name_element_object_found(self, name_param, wait_time=10):
-        wait = WebDriverWait(self.browser, wait_time)
+        wait = WebDriverWait(self, wait_time)
         wait.until(EC.visibility_of_element_located((By.NAME, name_param)))
 
     def wait_until_partial_link_text_element_object_found(self, partial_link_text, wait_time=10):
-        wait = WebDriverWait(self.browser, wait_time)
+        wait = WebDriverWait(self, wait_time)
         wait.until(EC.visibility_of_element_located(
             (By.PARTIAL_LINK_TEXT, partial_link_text)))
 
     def wait_until_class_name_element_object_found(self, class_name, wait_time=10):
-        wait = WebDriverWait(self.browser, wait_time)
+        wait = WebDriverWait(self, wait_time)
         wait.until(EC.visibility_of_element_located(
             (By.CLASS_NAME, class_name)))
 
     def wait_until_id_element_object_found(self, id_object, wait_time=10):
-        wait = WebDriverWait(self.browser, wait_time)
+        wait = WebDriverWait(self, wait_time)
         wait.until(EC.visibility_of_element_located((By.ID, id_object)))
 
     def wait_until_partial_link_text_object_found(self, id_object, wait_time=10):
-        wait = WebDriverWait(self.browser, wait_time)
+        wait = WebDriverWait(self, wait_time)
         wait.until(EC.visibility_of_element_located(
             (By.PARTIAL_LINK_TEXT, id_object)))
 
@@ -138,5 +139,5 @@ class LoginClass(object):
 
 
 if __name__ == '__main__':
-    fetching_token = LoginClass()
+    fetching_token = MyChrome()
     fetching_token.logging_in('https://www.google.com')
