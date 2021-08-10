bd4d95dd189b137b6094ee4af319f77683a57ceb
Senh Mo Chuang <senhmo@gmail.com>
2021-08-10 13:45:03 -0500

    code cleanup

--- Webscraper.py
+++ Webscraper.py
@@ -9,50 +9,88 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from pathlib import Path
+from selenium.webdriver.common.keys import Keys
 import os
-from time import sleep
+from abc import ABC
 
-def delay_before(delay):
-    def wrap(f):
-        def wrapped_f(*args):
-            sleep(delay)
-            f(*args)
-        return wrapped_f
-    return wrap
+class SeleniumAddons(ABC):
 
-def delay_after(delay):
-    def wrap(f):
-        def wrapped_f(*args):
-            f(*args)
-            sleep(delay)
-        return wrapped_f
-    return wrap
+    def wait_until_css_element_object_found(self, css_param, wait_time=10):
+        wait = WebDriverWait(self.browser, wait_time)
+        wait.until(EC.visibility_of_element_located(
+            (By.CSS_SELECTOR, css_param)))
 
+    def scroll_down_one_page(self, lines_down):
+        self.browser.execute_script(
+            "window.scrollTo(0, " + str(lines_down) + ")")
 
-class DelayBefore:
-    def __init__(self, delay):
-        self.delay = delay
+    def drag_and_drop(self, source_element_object, destination_element_object):
+        ActionChains(self.browser).drag_and_drop(
+            source_element_object, destination_element_object).perform()
 
-    def __call__(self, func):
-        def wrapper(*args):
-            sleep(self.delay)
-            func_return = func(*args)
-            return func_return
-        return wrapper
+    def hover_over(self, element_object):
+        ActionChains(self.browser).move_to_element(element_object).perform()
 
+    def is_present(self, element_object):
+        try:
+            # browser.find_element_by_css_selector("div")
+            if element_object.is_displayed():
+                return True
+        except:
+            return False
 
-class MyChrome(Chrome):
+    def update_browser(self):
+        for handle in self.browser.window_handles:
+            self.browser.switch_to.window(handle)
+
+    def wait_until_name_element_object_found(self, name_param, wait_time=10):
+        wait = WebDriverWait(self.browser, wait_time)
+        wait.until(EC.visibility_of_element_located((By.NAME, name_param)))
+
+    def wait_until_partial_link_text_element_object_found(self, partial_link_text, wait_time=10):
+        wait = WebDriverWait(self.browser, wait_time)
+        wait.until(EC.visibility_of_element_located(
+            (By.PARTIAL_LINK_TEXT, partial_link_text)))
+
+    def wait_until_class_name_element_object_found(self, class_name, wait_time=10):
+        wait = WebDriverWait(self.browser, wait_time)
+        wait.until(EC.visibility_of_element_located(
+            (By.CLASS_NAME, class_name)))
+
+    def wait_until_id_element_object_found(self, id_object, wait_time=10):
+        wait = WebDriverWait(self.browser, wait_time)
+        wait.until(EC.visibility_of_element_located((By.ID, id_object)))
+
+    def wait_until_partial_link_text_object_found(self, id_object, wait_time=10):
+        wait = WebDriverWait(self.browser, wait_time)
+        wait.until(EC.visibility_of_element_located(
+            (By.PARTIAL_LINK_TEXT, id_object)))
+
+    def multi_select_in_list(self, element_objects, labels):
+        for option in element_objects:
+            if option.text in labels:
+                option.click()
+
+    def select_in_list(self, element_objects, labels):
+        for option in element_objects:
+            if option.text in labels:
+                option.click()
+                break
+
+class LoginClass(SeleniumAddons):
 
     def __init__(self, incognito=True, headless=False, brave=False):
-        
         options = ChromeOptions()
+
+        # https://stackoverflow.com/questions/64927909/failed-to-read-descriptor-from-node-connection-a-device-attached-to-the-system
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
         options.add_argument("disable-extensions")
         if incognito:
             options.add_argument("incognito")
         if headless:
             options.add_argument("headless")
-        # https://stackoverflow.com/questions/64927909/failed-to-read-descriptor-from-node-connection-a-device-attached-to-the-system
-        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
         # options.add_argument("disable-gpu")
         # options.add_argument('window-size=1200x1200')
         # options.add_argument("remote-debugging-port=9222")
@@ -70,74 +108,17 @@ class MyChrome(Chrome):
                 options.binary_location = "/Applications/Brave Browser.app/Contents/MacOS/Brave Browser"
             path_to_chrome = str(
                 Path('./ChromeDrivers/Linux/chromedriver').absolute())
-        super().__init__(path_to_chrome, options=options)
+        self.browser = Chrome(path_to_chrome, options=options)
 
     def logging_in(self, url):
-        self.get(url)
-
-    def wait_until_css_element_object_found(self, css_param, wait_time=10):
-        wait = WebDriverWait(self, wait_time)
-        wait.until(EC.visibility_of_element_located(
-            (By.CSS_SELECTOR, css_param)))
-
-    def scroll_down_one_page(self, lines_down):
-        self.execute_script(
-            "window.scrollTo(0, " + str(lines_down) + ")")
-
-    def drag_and_drop(self, source_element_object, destination_element_object):
-        ActionChains(self).drag_and_drop(
-            source_element_object, destination_element_object).perform()
-
-    def hover_over(self, element_object):
-        ActionChains(self).move_to_element(element_object).perform()
-
-    def is_present(self, element_object):
-        try:
-            # browser.find_element_by_css_selector("div")
-            if element_object.is_displayed():
-                return True
-        except:
-            return False
-
-    def update_browser(self):
-        for handle in self.window_handles:
-            self.switch_to.window(handle)
-
-    def wait_until_name_element_object_found(self, name_param, wait_time=10):
-        wait = WebDriverWait(self, wait_time)
-        wait.until(EC.visibility_of_element_located((By.NAME, name_param)))
-
-    def wait_until_partial_link_text_element_object_found(self, partial_link_text, wait_time=10):
-        wait = WebDriverWait(self, wait_time)
-        wait.until(EC.visibility_of_element_located(
-            (By.PARTIAL_LINK_TEXT, partial_link_text)))
-
-    def wait_until_class_name_element_object_found(self, class_name, wait_time=10):
-        wait = WebDriverWait(self, wait_time)
-        wait.until(EC.visibility_of_element_located(
-            (By.CLASS_NAME, class_name)))
-
-    def wait_until_id_element_object_found(self, id_object, wait_time=10):
-        wait = WebDriverWait(self, wait_time)
-        wait.until(EC.visibility_of_element_located((By.ID, id_object)))
-
-    def wait_until_partial_link_text_object_found(self, id_object, wait_time=10):
-        wait = WebDriverWait(self, wait_time)
-        wait.until(EC.visibility_of_element_located(
-            (By.PARTIAL_LINK_TEXT, id_object)))
-
-    def multi_select_in_list(self, element_objects, labels):
-        for option in element_objects:
-            if option.text in labels:
-                option.click()
-
-    def select_in_list(self, element_objects, labels):
-        for option in element_objects:
-            if option.text in labels:
-                option.click()
-                break
+        self.browser.get(url)
 
 
 if __name__ == '__main__':
-    fetching_token = MyChrome()
+    fetching_token = LoginClass()
     fetching_token.logging_in('https://www.google.com')
+    fetching_token.wait_until_name_element_object_found('q')
+    elem = fetching_token.browser.find_element_by_name('q')
+    elem.send_keys('hello')
+    elem.send_keys(Keys.RETURN)
+
